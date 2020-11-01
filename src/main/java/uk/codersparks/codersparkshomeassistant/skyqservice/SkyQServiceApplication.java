5760fac70fc66518061612c0e53b9b8fffa02a74
codersparks <codersparks@gmail.com>
2020-11-01 16:27:33 +0000

    Initial commit

---
+++ src/main/java/uk/codersparks/codersparkshomeassistant/skyqservice/SkyQServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.codersparks.codersparkshomeassistant.skyqservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyQServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyQServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/codersparks/codersparkshomeassistant/skyqservice/SkyQServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.codersparks.codersparkshomeassistant.skyqservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyQServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
